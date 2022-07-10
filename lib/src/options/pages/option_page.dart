2e5bf74bdef5afb6ebee4503ca5c29fdef18847c
RaulSangosto <48175562+RaulSangosto@users.noreply.github.com>
2022-07-10 10:55:18 +0200

    Feature: options redesign

--- lib/src/options/pages/option_page.dart
+++ lib/src/options/pages/option_page.dart
@@ -11,6 +11,7 @@ class OptionsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: darkColor,
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 50.0),
@@ -26,7 +27,7 @@ class OptionsPage extends StatelessWidget {
                 children: [
                   Text(
                     "Settings",
-                    style: deckTextStyle.copyWith(color: darkColor),
+                    style: deckTextStyle,
                   ),
                 ],
               ),
@@ -36,6 +37,7 @@ class OptionsPage extends StatelessWidget {
                 flex: 2,
               ),
               ElevatedButton(
+                style: secondaryButton,
                 onPressed: () {
                   GoRouter.of(context).pop();
                 },
@@ -64,6 +66,7 @@ class VolumeControls extends StatelessWidget {
           children: [
             SliderItem(
               text: "Music",
+              textColor: white,
               onChanged: (double value) {
                 BlocProvider.of<SoundBloc>(context).add(SetMusicVolume(value));
               },
@@ -74,6 +77,7 @@ class VolumeControls extends StatelessWidget {
                 },
                 icon: Icon(
                     size: 35,
+                    color: white,
                     state.musicMute()
                         ? Icons.music_off_rounded
                         : Icons.music_note_rounded),
@@ -81,6 +85,7 @@ class VolumeControls extends StatelessWidget {
             ),
             SliderItem(
               text: "Sound",
+              textColor: white,
               onChanged: (double value) {
                 BlocProvider.of<SoundBloc>(context).add(SetSoundVolume(value));
               },
@@ -91,23 +96,47 @@ class VolumeControls extends StatelessWidget {
                 },
                 icon: Icon(
                     size: 35,
+                    color: white,
                     state.soundMute()
                         ? Icons.volume_off_rounded
                         : Icons.volume_up_rounded),
+              ),
+            ),
+            SliderItem(
+              text: "Dialog",
+              textColor: white,
+              onChanged: (double value) {
+                BlocProvider.of<SoundBloc>(context).add(SetHelperVolume(value));
+              },
+              value: state.helperVolume,
+              icon: IconButton(
+                onPressed: () {
+                  BlocProvider.of<SoundBloc>(context).add(ToggleHelperVolume());
+                },
+                icon: Icon(
+                    size: 35,
+                    color: white,
+                    state.helperMute()
+                        ? Icons.voice_over_off_rounded
+                        : Icons.record_voice_over_rounded),
               ),
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Expanded(
-                    child: Text(
-                  "Credits",
-                  style: Theme.of(context).textTheme.headline6,
-                )),
+                    child: Text("Credits",
+                        style: Theme.of(context)
+                            .textTheme
+                            .headline6
+                            ?.copyWith(color: white))),
                 IconButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    GoRouter.of(context).pushNamed("credits");
+                  },
                   icon: const Icon(
                     Icons.badge_rounded,
+                    color: white,
                     size: 35,
                   ),
                 )
@@ -127,9 +156,11 @@ class SliderItem extends StatelessWidget {
     required this.onChanged,
     required this.text,
     required this.icon,
+    this.textColor = darkColor,
   }) : super(key: key);
 
   final String text;
+  final Color textColor;
   final Widget icon;
   final double value;
   final ValueChanged<double>? onChanged;
@@ -146,7 +177,10 @@ class SliderItem extends StatelessWidget {
             children: [
               Text(
                 text,
-                style: Theme.of(context).textTheme.headline6,
+                style: Theme.of(context)
+                    .textTheme
+                    .headline6
+                    ?.copyWith(color: textColor),
               ),
               Container(child: Slider(value: value, onChanged: onChanged)),
             ],
